4895a0db3816683bfbf0a73bc118831817846ad4
Kozma Norbert <norbert.kozma96@gmail.com>
2021-03-04 12:12:29 +0100

    Remove commented lines

--- toolsQA/tests/UploadFile.ts
+++ toolsQA/tests/UploadFile.ts
@@ -9,17 +9,3 @@ test('Upload File', async (t) => {
 	await t.setFilesToUpload(Selector('#uploadFile'), yourFixturePath);
 	await t.expect(Selector('#uploadedFilePath').innerText).eql(uploadedFilePath);
 });
-
-/* import 'cypress-file-upload'
-
-describe ('', function() {
- 
-    it('Upload file', function() {   
-       cy.visit('/upload-download', {failOnStatusCode: false})
-      
-       const yourFixturePath = '../upload/sampleFile'
-       cy.get('#uploadFile').attachFile(yourFixturePath)
-       
-       cy.get('#uploadedFilePath').should('have.text', 'C:\\fakepath\\sampleFile')
-   })  
-}) */
